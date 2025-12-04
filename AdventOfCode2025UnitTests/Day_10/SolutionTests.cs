52cbf26ab9831a920c2650f36550f0e6ea107046
Szymon Wesolowski <szymon.wesolowski1@sainsburys.co.uk>
2025-12-04 09:02:19 +0000

    add missing tests

---
+++ AdventOfCode2025UnitTests/Day_10/SolutionTests.cs
@@ -0,0 +1,66 @@
+using FluentAssertions;
+
+namespace AdventOfCode2025UnitTests.Day_10;
+
+public class SolutionTests
+{
+    [Fact]
+    public void Test_Solve1()
+    {
+        // Arrange
+        var solution = new AdventOfCode2025.Day_10.Solution1();
+        string[] input =
+        [
+            "",
+        ];
+
+        // Act
+        long result = solution.Solve(input);
+
+        // Assert
+        result.Should().Be(0);
+    }
+
+    [Fact]
+    public void Real_Solve_01()
+    {
+        // Arrange
+        var solution = new AdventOfCode2025.Day_10.Solution1();
+
+        // Act
+        long result = solution.Start();
+
+        // Assert
+        result.Should().Be(0);
+    }
+
+    [Fact]
+    public void Test_Solve2()
+    {
+        // Arrange
+        var solution = new AdventOfCode2025.Day_10.Solution2();
+        string[] input =
+        [
+            "",
+        ];
+
+        // Act
+        long result = solution.Solve(input);
+
+        // Assert
+        result.Should().Be(0);
+    }
+
+    [Fact]
+    public void Real_Solve_02()
+    {
+        // Arrange
+        var solution = new AdventOfCode2025.Day_10.Solution2();
+
+        // Act
+        long result = solution.Start();
+
+        // Assert
+        result.Should().Be(0);
+    }
+}
