00394d7bf6db1008d2eca63f8e2cfe71f951b3b8
Szymon Wesolowski <szymon.wesolowski1@sainsburys.co.uk>
2025-12-11 13:11:57 +0000

    day 11 part 2 top down

--- AdventOfCode2025/Day_11/Solution2.cs
+++ AdventOfCode2025/Day_11/Solution2.cs
@@ -2,24 +2,27 @@ namespace AdventOfCode2025.Day_11;
 
 public class Solution2
 {
+    Dictionary<string, List<string>> graph = new();
+    Dictionary<(string, bool, bool), long> memo = new();
+    string point1 = "dac";
+    string point2 = "fft";
+    string target = "out";
+
     public string[] GetInput()
     {
         return System.IO.File.ReadAllLines("Day_11/Input.txt");
     }
 
-    public int Start()
+    public long Start()
     {
         var input = GetInput();
         return Solve(input);
     }
 
-    public int Solve(string[] input)
+    public long Solve(string[] input)
     {
         string startingPoint = "svr";
-        string point1 = "dac";
-        string point2 = "fft";
-        string target = "out";
-        var graph = new Dictionary<string, List<string>>();
+
         foreach (var line in input)
         {
             var parts = line.Split(": ");
@@ -28,33 +31,36 @@ public class Solution2
             graph[node] = neighbors.Select(x => x.Trim()).ToList();
         }
 
-        int result = 0;
-        var visited = new HashSet<string>();
+        return DFS(startingPoint, false, false);
+    }
 
-        void DFS(string currentNode)
+    long DFS(string currentNode, bool visited1, bool visited2)
+    {
+        long result = 0;
+
+        if (currentNode == target)
         {
-            if (currentNode == target)
+            if(visited1 && visited2)
             {
-                if (visited.Contains(point1) && visited.Contains(point2))
-                {
-                    result++;
-                }
-                return;
+                return 1;
             }
-
-            foreach (var neighbor in graph[currentNode])
-            {
-                if (visited.Contains(neighbor)) continue;
-
-                visited.Add(neighbor);
-                DFS(neighbor);
-                visited.Remove(neighbor);
-            }
+            return 0;
         }
 
-        visited.Add(startingPoint);
-        DFS(startingPoint);
+        visited1 |= currentNode == point1;
+        visited2 |= currentNode == point2;
 
+        if (memo.ContainsKey((currentNode, visited1, visited2)))
+        {
+            return memo[(currentNode, visited1, visited2)];
+        }
+
+        foreach (var neighbor in graph[currentNode])
+        {
+            result += DFS(neighbor, visited1, visited2);
+        }
+
+        memo[(currentNode, visited1, visited2)] = result;
         return result;
     }
 }
